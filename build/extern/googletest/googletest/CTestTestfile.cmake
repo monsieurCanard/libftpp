6dab42e572abd3b3b0301c629da62324e0bdc034
MonsieurC <anthony.gab@outlook.fr>
2025-09-02 18:31:50 +0200

    (new) thread functions DONE

--- build/extern/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anthony/Documents/libftpp/extern/googletest/googletest
-# Build directory: /home/anthony/Documents/libftpp/build/extern/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
